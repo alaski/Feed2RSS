bf0e6ba90aee8576510eb40078bddf6fb27f4acc
Andrew Laski <alaski@gmail.com>
2012-04-05 09:03:49 -0400

    Signal passing in bash script

--- runapp.py
+++ runapp.py
@@ -1,14 +1,8 @@
 import os
-import signal
 
 from paste.deploy import loadapp
 from waitress import serve
 
-def graceful_shutdown(signum, frame):
-    print 'Shutdown called with signal {0}'.format(signum)
-    exit()
-
-signal.signal(signal.SIGTERM, graceful_shutdown)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
